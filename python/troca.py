af0bb229c30cbd840ff5a3fb35d7f42b0f1635fa
Mauricio P. Pires <mauricioppires@gmail.com>
2023-05-11 15:03:04 -0300

    Create troca.py

---
+++ python/troca.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+# Para trocar o valor de duas variaveis em Python é bastante simples!
+# veja:
+
+a = 5
+b = 8
+
+print("Valores das variaveis: a = {} b = {}".format(a,b))
+
+# observe aqui!
+a,b = b,a
+
+print("Valores das variaveis: a = {} b = {}".format(a,b))
